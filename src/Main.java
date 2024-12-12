829eb3b8010a02216a87c040b35dcb04858890a4
Leroy <LeraRu@tut.by>
2024-12-12 13:41:42 +0300

    Strings is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = "Иванов Иван Иванович";
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
+
+        //Задача 2
+        String fullNameNew = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameNew);
+
+        //Задача 3
+        //String fullName = "Иванов Семён Семёнович";
+        //String fullNameNew = fullName.replace("ё","е");
+        //System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNew);
+
 
 
     }
+
 }
